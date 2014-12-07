dd46d04ca6b981678a044efc7c212b115fe46c56
ihalapathirana <>
2014-12-07 17:49:25 +0530

    with more images

---
+++ TicTac/src/tictac/TicTac.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictac;
+
+/**
+ *
+ * @author HP
+ */
+public class TicTac {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        Front front=new Front();
+        front.setVisible(true);
+        front.setTitle("Tic Tac Toe");
+        front.load.initialize();
+        front.load.setVisible(false);
+        
+    }
+}
